752dc3c670e9f87a4f2c5d62d352d45f81f5cfb6
WilliamStanford <54320229+WilliamStanford@users.noreply.github.com>
2024-03-25 14:15:43 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,9 +6,35 @@ import numpy as np
 import cv2
 from PIL import Image
 import warnings
-
+import argparse
 
 warnings.filterwarnings('ignore')
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="arguments for mp3 to video generation")
+
+    parser.add_argument(
+        "--rec_model_path", type=str, default='./deeperaser.pth',
+        help="The path to necessary version of deeperaser.pth"
+    )
+    
+    parser.img_path(
+        "--img_path", type=str, default='./input_imgs/input.jpg',
+        help="Path to image for text removal"
+    )
+
+    parser.mask_path(
+        "--mask_path", type=str, default='./input_imgs/mask.jpg',
+        help="Path to precomputed mask image used to specify text removal locations"
+    )
+
+    parser.save_path(
+        "--save_path", type=str, default='./output_imgs/output.jpg',
+        help="Path to save de-texted image"
+    )
+
+    user_args = parser.parse_args()
+    return user_args
 
 
 def reload_rec_model(model, path=""):
@@ -26,33 +52,24 @@ def reload_rec_model(model, path=""):
         return model
 
 
-def rec(rec_model_path, img_path, save_path):
-    # print(torch.__version__)
-
+def rec(rec_model_path, img_path, mask_path, save_path):
 
     net = DeepEraser().cuda()
-
-
     reload_rec_model(net, rec_model_path)
-
     net.eval()
 
-    img = np.array(Image.open(img_path+'input.jpg'))[:, :, :3]   
-    mask = np.array(Image.open(img_path+'mask.png'))[:, :]
-    
+    img = np.array(Image.open(img_path))[:, :, :3]   
+    mask = np.array(Image.open(mask_path))[:, :]
 
     im = torch.from_numpy(img / 255.0).permute(2, 0, 1).float()
     mask = torch.from_numpy(mask / 255.0).unsqueeze(0).float()
     
     with torch.no_grad():
     
-        name = 'output'
         pred_img = net(im.unsqueeze(0).cuda(), mask.unsqueeze(0).cuda())
         pred_img[-1] = torch.clamp(pred_img[-1], 0, 1)
-        
         out = (pred_img[-1][0]*255).permute(1, 2, 0).cpu().numpy().astype(np.uint8)
-        
-        cv2.imwrite(save_path + name + '.png', out[:,:,::-1])
+        cv2.imwrite(save_path, out[:,:,::-1])
             
 
 def get_parameter_number(net):
@@ -62,10 +79,8 @@ def get_parameter_number(net):
 
 
 def main():
-    rec_model_path = './deeperaser.pth'
-    img_path = './input_imgs/'
-    save_path =  './output_imgs/'
-    rec(rec_model_path,img_path,save_path)
+    user_args = parse_args()
+    rec(user_args.rec_model_path, user_args.img_path, user_args.mask_path, user_args.save_path)
 
 
 if __name__ == "__main__":
